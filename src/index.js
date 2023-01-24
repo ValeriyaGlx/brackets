f784460750bc62afc87d5cb2f588866d997ba5c1
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-01-25 01:18:47 +0400

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+    const pairs = {};
+    const opens = []
+    let stuck = [];
+    
+    for (let i=0; i< bracketsConfig.length; i++){
+    pairs[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    opens.push(bracketsConfig[i][0])
+    }
+    
+    for(let j=0; j<str.length; j++){
+        if(opens.includes(str[j])){
+            stuck.push(str[j])
+        }  else {
+            if(stuck.length===0) return false
+        }
+    
+        let currentSign = stuck[stuck.length -1]
+     if(pairs[str[j]]===currentSign){
+        stuck.pop()
+     }
+    }
+    console.log(stuck);
+    return stuck.length === 0
+      }
 }
